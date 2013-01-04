20af687746bc90ce59245ab05c178beb805488a7
evan <evan.jacobs2@gmail.com>
2013-01-04 18:12:24 -0500

    Moved examples to own directory Started implementing dialog position options

--- typetoconfirm.js
+++ typetoconfirm.js
@@ -9,7 +9,8 @@
             'message'         : 'Please type the word \'{{t}}\' to confirm',    // {{t}} is replace with the textotype setting
             'arrowposition'   : 'middle',                                       // top, bottom, middle
             'position'        : 'above',                                        // above, below, left, right, auto
-            'persist'         : false
+            'arrowmargintop'  : 4,
+            'dialogmargintop' : 8
         }, options);
 
 
@@ -17,7 +18,8 @@
             'nextletterindex'   : 0,
             'instantiated'      : false,
             'submitform'        : false,
-            'confirmedsubmit'   : false
+            'confirmedsubmit'   : false,
+            'nextlettertoyype'  : null
         }
 
         var methods = {
@@ -29,6 +31,7 @@
                     newConfirmStr += '*';
                 }
                 lettersContainer.text(newConfirmStr);
+
                 variables.instantiated = true;
             }
 
@@ -47,7 +50,7 @@
                 variables.submitform = true;
             }
 
-            $(this).after('<div class="confirmDialog '+ settings.class +'" style="display: none">Confirm here</div>');
+            $(this).after('<div class="confirmDialog '+ settings.class +'" style="display: none"></div>');
             this.dialogElement = $(this).next('.confirmDialog');
             var onClickCode = $(this).attr('onclick');
             if (onClickCode != undefined) {
@@ -55,7 +58,8 @@
                 $(this).removeAttr('onclick');
             }
 
-            var newHtml = '<input type="text" title="'+ settings.texttotype +'" style="width: 0; height: 0; float: left;">';
+            var newHtml = '<div class="arrowContainer"></div>';
+            newHtml += '<input type="text" title="'+ settings.texttotype +'" style="width: 0; height: 0; float: left;">';
             newHtml += '<div class="untypedletterscontainer">';
             for ( var i = 0; i < settings.texttotype.length; i++ ) {
                 newHtml += '*';
@@ -80,23 +84,54 @@
                 $('.confirmDialog').hide();
                 dialogElement.show();
 
-                if (!settings.persist || !variables.instantiated) {
-                    methods.reset(dialogElement.find('.untypedletterscontainer'));
+                methods.reset(dialogElement.find('.untypedletterscontainer'));
+
+
+                // copy start
+
+                var buttonPosition = $(this).position();
+                var arrowContainerElement = dialogElement.find("> .arrowContainer")
+                var arrowSize = arrowContainerElement.height();
+
+                if (settings.arrowposition == 'middle') {
+                    dialogElement.css('margin-left',  ((dialogElement.width() / 2) - ($(this).width() / 2)) * (-1) );
+
+                    arrowContainerElement.css('margin-left', (dialogElement.outerWidth() / 2) - (arrowSize / 2) );
+
+                }
+                else if (settings.arrowposition == 'left') {
+                    dialogElement.css('left', buttonPosition.left);
+                    arrowContainerElement.css('margin-left', ( ($(this).outerWidth() / 2) - (arrowSize) )  );
+                }
+                else if (settings.arrowposition == 'right') {
+                    dialogElement.css('margin-left', (-1) * (dialogElement.outerWidth() - $(this).outerWidth())  );
+                    arrowContainerElement.css('margin-left', (dialogElement.outerWidth() - (($(this).outerWidth() / 2) + (arrowSize))  ) );
                 }
 
 
-                var nextLetterToType = settings.texttotype.charAt(variables.nextletterindex);
+                dialogElement.css('top', buttonPosition.top + $(this).height() + arrowSize + settings.dialogmargintop);
+
+
+                var arrowContainerTop = (-arrowSize) - settings.arrowmargintop ;
+
+
+                arrowContainerElement.css('top', arrowContainerTop + 'px');
+
+                // copy end
+
+
+                variables.nextlettertoyype = settings.texttotype.charAt(variables.nextletterindex);
                 dialogElement.find('input[type=text]').focus().keypress(function(e) {
 
                     var typedChar = String.fromCharCode(e.which);
                     if (!settings.casesensitive) {
                         typedChar = typedChar.toLowerCase();
-                        nextLetterToType = nextLetterToType.toLowerCase();
+                        variables.nextlettertoyype = variables.nextlettertoyype.toLowerCase();
                     }
 
-                    if (typedChar == nextLetterToType) {
+                    if (typedChar == variables.nextlettertoyype) {
                         variables.nextletterindex++;
-                        nextLetterToType = settings.texttotype.charAt(variables.nextletterindex);
+                        variables.nextlettertoyype = settings.texttotype.charAt(variables.nextletterindex);
                         var newConfirmStr = settings.texttotype.substr(0, variables.nextletterindex);
 
                         for (var i = variables.nextletterindex; i < settings.texttotype.length; i++) {
@@ -127,7 +162,7 @@
     };
 
     // Hide all on esc
-    $(document).keyup(function(e) {
+    $(document).keydown(function(e) {
         if (e.keyCode == 27) {
             $('.confirmDialog').hide();
         }
